f6434937e53178237fc82e6f77842c982baf2f0b
Aman Patel <apofficial2002@gmail.com>
2022-09-16 16:40:41 +0530

    already solved

--- dynamic-programing/Count_the_number_of_subset_with_a_given_difference.cpp
+++ dynamic-programing/Count_the_number_of_subset_with_a_given_difference.cpp
@@ -1,2 +1,13 @@
 // Count the number of subset with a given difference
+#include<bits/stdc++.h>
+using namespace std;
 
+// this question is same as target sum
+// # already solved
+
+
+int main()
+{
+    vector<int> arr = {5, 2, 6, 4};
+    return 0;
+}
