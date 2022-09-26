f4367491113f177c86ba16d11ccbd61e53c154cf
Aman Patel <apofficial2002@gmail.com>
2022-09-26 19:20:42 +0530

    commit

--- dynamic-programing/mcm/Evaluate_Expression_to_True_Boolean_Parenthesization.cpp
+++ dynamic-programing/mcm/Evaluate_Expression_to_True_Boolean_Parenthesization.cpp
@@ -36,7 +36,37 @@ class Solution{
         return ans;
     }
 };
-
+class SolutionRecursive{
+    int solve(string s, int i, int j, bool isTrue){
+        if(i>j)return 0;
+        else if(i==j){
+            if(isTrue)return s[i]=='T';
+            return s[i]=='F';
+        }
+        // else if(dp[i][j][isTrue]!=-1)return dp[i][j][isTrue];
+        int ans = 0;
+        for(int k = i+1; k<j; k=k+2){
+            int lt = solve(s, i, k-1, true);
+            int lf = solve(s, i, k-1, false);
+            int rt = solve(s, k+1, j, true);
+            int rf = solve(s, k+1, j, false);
+            if(s[k]=='&'){
+                if(isTrue)ans+=(lt*rt);
+                else ans+=((lf*rf) + (lf*rt) + (lt*rf));
+            }
+            else if(s[k]=='|'){
+                if(isTrue) ans+= ((lt*rt) + (lt*rf) + (lf*rt));
+                else ans+= (lf*rf);
+            }
+            else{
+                if(isTrue) ans+=((lt*rf)+(lf*rt));
+                else ans+=((lt*lf)+(rf*rt));
+            }
+        }
+        // dp[i][j][isTrue] = ans;
+        return ans;
+    }
+};
 
 int main(){
     return 0;
